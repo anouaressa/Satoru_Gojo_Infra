573436a8e40f7562095b91cb000ee578af1f34ec
Anouar Essabbari <89299571+anouaressa@users.noreply.github.com>
2023-10-26 14:17:02 +0000

    Update install_jenkins.sh

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt update 
-sudo apt update
 sudo apt install fontconfig openjdk-17-jre
 
 #jenkins
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
 /etc/apt/sources.list.d/jenkins.list > /dev/null
