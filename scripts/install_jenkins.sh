d7f10d806ac54230ebc19aa52d7512bce23e30df
Anouar Essabbari <89299571+anouaressa@users.noreply.github.com>
2023-10-26 14:16:18 +0000

    Update install_jenkins.sh

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 sudo apt update 
-
-# Install Java 
-sudo apt install openjdk-11-jdk -y
-
-#Add the repository key to the system
-sudo apt wget -q -O - https://pkg.jenkins.io/debianstable/jenkins.io.key | sudo apt-key add -
-
-
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
 sudo apt update
+sudo apt install fontconfig openjdk-17-jre
 
-sudo apt install jenkins
-
+#jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
 sudo systemctl start jenkins
-
 sudo systemctl enable jenkins
-
-sudo systemctl status jenkins
